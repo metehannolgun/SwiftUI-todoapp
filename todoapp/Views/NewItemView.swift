a9ba58a2642ab64c8719cfd4ce615044fb76d318
metehannolgun <metehannolgun@gmail.com>
2025-02-10 23:06:19 +0300

    -todolist' e eklenecek görev için view düzenlendi

--- todoapp/Views/NewItemView.swift
+++ todoapp/Views/NewItemView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @Binding var newItemPresented: Bool
+    @StateObject var viewModel = NewItemViewViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Yeni Görev")
+                .font(.title)
+                .bold()
+                .padding(.top,100)
+            Form{
+                TextField("Başlık", text:$viewModel.title)
+                DatePicker("Bitiş Tarihi", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                BigButton(title:"Kaydet"){
+                    if viewModel.canSave{
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+                }
+            }
+            .alert(isPresented: $viewModel.showAlert, content: {
+                Alert(
+                    title: Text("Hata"),
+                    message: Text("Lütfen verilerin doğruluğunu kontrol edin."))
+            })
+        }
     }
 }
 
 #Preview {
-    NewItemView()
+    NewItemView(newItemPresented: Binding(get:{
+        return true
+    }, set:{ _ in
+    }))
+       
 }
